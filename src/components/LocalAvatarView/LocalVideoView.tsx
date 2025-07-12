712c2fbaf4cf92620fc4df7d45e913d61e77beb5
Amit Shah <amit.shah@coinbase.com>
2025-07-12 02:48:14 -0400

    4:3

--- src/components/LocalAvatarView/LocalVideoView.tsx
+++ src/components/LocalAvatarView/LocalVideoView.tsx
@@ -456,8 +456,8 @@ export const LocalVideoView = ({ onCanvasStreamChanged }: Props) => {
     createLocalVideoTrack({
       facingMode: "user",
       resolution: { 
-        width: 1080, 
-        height: 1920, 
+        height: 960, 
+        width: 720, 
         frameRate: 60 
       },
     }).then((t) => {
